41da4d94ceb23659924cfeef950d814b1f2e65e6
andrecosta8224 <andrecosta8224@gmail.com>
2022-04-10 11:03:05 -0300

    update

---
+++ HTTP/app.js
@@ -0,0 +1,7 @@
+let http = require("http");
+
+http.createServer(function(req, res){
+  res.end("hello world!");
+}).listen(8081)
+
+console.log("Server is running");
---
+++ modulos/modulos.js
@@ -0,0 +1,9 @@
+let divFunc = require("./div");
+let subFunc = require("./sub");
+let multFunc = require("./mult")
+let somaFunc = require("./soma")
+
+console.log(divFunc(4, 2));
+console.log(subFunc(6, 3));
+console.log(multFunc(2, 2));
+console.log(somaFunc(4, 1));
