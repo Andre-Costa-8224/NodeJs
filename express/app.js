a3a275af86c6bccb4d4028b7451921dd26324ab2
andrecosta8224 <andrecosta8224@gmail.com>
2022-04-14 22:38:52 -0300

    update

--- express/app.js
+++ express/app.js
@@ -1,4 +1,31 @@
 const express = require("express")
+const Sequelize = require("sequelize")
+
+const sequelize = new Sequelize("teste", "u0_a1269", "", {
+				host: "localhost",
+				dialect: "mariadb",
+				port: "3306"
+})
+
+
+sequelize.authenticate().then(() => console.log("conectado com sucesso!")). catch ((err) => console.log("erro: "+err))
+
+/*
+const produtos = sequelize.define("produto", {
+				nome: {
+								type: Sequelize.STRING
+				},
+				categoria: {
+								type: Sequelize.STRING
+				},
+				descricao: {
+								type: Sequelize.TEXT
+				}
+})
+
+produtos.sync({force: true})
+*/
+
 const app = express()
 
 app.get("/", function(req,res){
